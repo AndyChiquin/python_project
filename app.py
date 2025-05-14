b02a5f2e07f60360a710126747d62f00c6cd0406
AndyChiquin <andychiquindev@gmail.com>
2025-05-14 17:54:37 -0500

    Agrega Flask app y Dockerfile para despliegue en DigitalOcean

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello, this is a project in Python!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, this is a project in Python using Flask on DigitalOcean!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
