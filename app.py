06f01668487552498fa17fb260e16dbe0a7c72a5
AndyChiquin <andychiquindev@gmail.com>
2025-05-25 14:46:07 -0500

    Test: trigger auto-deploy to DigitalOcean

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, this is a project in Python using Flask on DigitalOcean1!"
+    return "Hello from GitHub Actions and DigitalOcean!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
