828fadeddc1b7faf583d9efc3a0be1c7247d64b6
AndyChiquin <andychiquindev@gmail.com>
2025-05-25 13:48:39 -0500

    Test Github Actions build

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, this is a project in Python using Flask on DigitalOcean!"
+    return "Hello, this is a project in Python using Flask on DigitalOcean1!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
